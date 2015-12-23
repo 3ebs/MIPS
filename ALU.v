32f73f2a8142e97e3b5377fb53b65807ff2a1104
wale-edx <waleed.edx@gmail.com>
2015-12-23 19:27:57 +0200

    Adding Shamt input
    Adding shamt input and changing the way we set the zero signal .

--- ALU.v
+++ ALU.v
@@ -1,6 +1,7 @@
-module ALU (opA , opB ,alu_control_signal , out ,carry,zero );
+module ALU (opA , opB ,shamt,alu_control_signal , out ,carry,zero );
 input [31:0] opA;
 input [31:0] opB;
+input [4:0] shamt;
 input [2:0] alu_control_signal;
 output reg [31:0] out;
 output reg carry ,zero;
@@ -18,31 +19,31 @@ begin
 case(alu_control_signal)
 	ADD: begin 
 			{carry,out}<= opA + opB ;
-			zero<= !(opA-opB);
+			zero<= ( out == 0 ) ? 1 : 0;
 		end
 	SUB: begin 
 			out <= opA - opB;
-			zero<= !(opA-opB);
+			zero<= ( out == 0 ) ? 1 : 0;
 		end	
 	SLL : begin 
-			out<= opA<<opB;
-			zero<= !(opA-opB);
+			out<= opA<<shamt;
+			zero<= ( out == 0 ) ? 1 : 0;
 		 end
 	NOR  :
 		begin
 			out <= !(opA | opB ) ;
-			zero<= !(opA-opB);
+			zero<= ( out == 0 ) ? 1 : 0;
 
 		end
 	AND :begin 
 			out <= opA & opB ;
-			zero<= !(opA-opB);
+			zero<= ( out == 0 ) ? 1 : 0;
 		end
 		
 	SLT :
 			begin 			
 				out <= (opA<opB)? 1 : 0 ;
-				zero<= !(opA-opB);
+				zero <= ( out == 0 ) ? 1 : 0;
 			end
 	endcase
 
