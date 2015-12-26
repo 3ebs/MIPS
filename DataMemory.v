521ceab2441dfffac9562a013e916dbb6933cc77
Mohamed Magdy <Mohamed Magdy>
2015-12-26 20:38:06 +0200

    DMem modification

--- DataMemory.v
+++ DataMemory.v
@@ -3,12 +3,15 @@ module DataMemory (addr, data_in, data_out, rd, wr);
 	input [31:0] addr;
 	input [31:0] data_in;
 	output reg [31:0] data_out;
-	reg [31:0] my_memory [0:1024]; 
+	reg [31:0] my_memory [0:1024]; 		 
+	reg [31:0] tempaddr;
 	always @ (*) begin
+		tempaddr = addr;
+		tempaddr /= 4;
 		if (wr &!rd) 
-			my_memory[addr] <=  data_in;
+			my_memory[tempaddr] <=  data_in;
 		else if  (!wr & rd) 
-			data_out =  my_memory[addr];
+			data_out =  my_memory[tempaddr];
 			$writememb ("D_memory.list", my_memory);
 	end	 
 	initial begin
