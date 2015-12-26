f934dbf68050bdd9ffc31e8a152c7dfbaba49517
wale-edx <waleed.edx@gmail.com>
2015-12-26 14:24:58 +0200

    Revert "Sho3'l Fanade2 ^^"
    This reverts commit a27fc7d8404d03e48ed0cfef6dc6eb02eeaa61ab.

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -10,12 +10,9 @@ module InstructionMemory(address, to_ctrl, to_rs, to_rt, to_rd, to_shamt, to_fun
 	reg [25:0]to_jump;
 	reg [15:0]to_sign_ext;
 	reg [31:0] my_memory [0:1024];
-	reg [31:0] tempreg;	
-	reg [31:0] tempaddr;
+	reg [31:0] tempreg;
 	always @ (address) begin
-	 	tempaddr = address;
-		tempaddr /= 4;
-		tempreg = my_memory[tempaddr];
+		tempreg = my_memory[address];
 		to_ctrl = tempreg[31:26];
 		to_rs = tempreg[25:21];
 		to_rt = tempreg[20:16];
@@ -23,9 +20,9 @@ module InstructionMemory(address, to_ctrl, to_rs, to_rt, to_rd, to_shamt, to_fun
 		to_shamt = tempreg[10:6]; 
 		to_function = tempreg[5:0];
 		to_jump = tempreg[25:0];
-		to_sign_ext = tempreg[15:0]; 
+		to_sign_ext = tempreg[15:0];
 	end
 	initial begin
-	  $readmemb ("I_memory.list", my_memory);
+		$readmemb ("memory.list", my_memory);
 	end
 endmodule
--- SLL2.v
+++ SLL2.v
@@ -1,15 +1,6 @@
-module shift_left_2(in, out);
+					
+module SLL_2(in, out);
 	input [31:0]in;
 	output [31:0]out;
 	assign out = in << 2;
-endmodule
-
-module jumpAddress(in, out, pc4);
-  input [25:0] in;
-  output reg [31:0] out; 
-  input [31:0]pc4;	
-  always @(in)
-		  begin
-			  out = (in << 2) | pc4;  
-		  end
 endmodule
